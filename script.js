8a540868818652754e429b020309b531309e53ab
khouilid <57531206+khouilid@users.noreply.github.com>
2020-04-09 22:21:25 +0100

    finish it

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
+const the_hour = document.querySelector("#the_hour");
+const the_min = document.querySelector("#the_min");
+const the_sc = document.querySelector("#the_sc")
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
 
@@ -18,12 +21,28 @@ const AIGUILLESEC = document.querySelector("#second");
 // Déplacer les aiguilles 
 function demarrerLaMontre() {
     let Datte = new Date();
-    let hh = Datte.getDate() * 30;
+    let hh = Datte.getHours() * 30;
     let mn = Datte.getMinutes() * 6;
     let sc = Datte.getSeconds() * 6;
     AIGUILLEHR.style.transform =  `rotateZ(${(hh) + (mn / 12)}deg)`;
     AIGUILLEMIN.style.transform = `rotateZ(${(mn)}deg)`;
     AIGUILLESEC.style.transform = `rotateZ(${(sc)}deg)`;
+    let H = Datte.getHours();
+    let m = Datte.getMinutes();
+    let s = Datte.getSeconds();
+    if (m < 10 ){
+        m = "0" + m;
+
+    }
+    if (s < 10 ){
+        s = "0" + s;
+
+    }
+
+    the_hour.textContent = H;
+    the_min.textContent = m;
+    the_sc.textContent = s;
+  
 
 
 
